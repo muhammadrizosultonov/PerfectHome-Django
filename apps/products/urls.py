9f5005b49aa9a6c3a2e8449fcf9b391aaf03c046
muhammadrizosultonov <muhammadrizosultonov33@gmail.com>
2026-03-27 06:14:04 +0500

    urls

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "products"
 
 urlpatterns = [
-    path('products/<str:slug>/', views.product_detail, name='detail'),
     path("", views.product_catalog, name="catalog"),
-    path("<slug:slug>/", views.product_detail, name="detail"),
+    path("<str:slug>/", views.product_detail, name="detail"),
 ]
